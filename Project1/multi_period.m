db93d0b0ad21e064d375141c0980a199f6d13a0a
yhyhyh <893132051@qq.com>
2017-03-19 15:41:05 -0400

    Three method compared
    The end of function plots the wealth curve of CVaR, MVO and benchmark

--- Project1/multi_period.m
+++ Project1/multi_period.m
@@ -1,94 +1,132 @@
-function  multi_period_hw2
+function  multi_period
 
-load hw2.mat; 
-n = size(Price,2);
-e = ones(n,1);	
+    load hw2.mat; 
+    n = size(Price,2);
+    e = ones(n,1);
 
-horizon = 4;
-start = 400;
-number_rebalances = 100;
+    horizon = 4;
+    start = 400;
+    number_rebalances = 100;
 
-number_of_samples = 100;
-sample_frequency = 2;
-r_w_f_o_y_e = .4;
-allowable_risk = 1;
-			
-trans_cost = .005;
-wealth = 10000;
-x0 = .3;
-x = (.7/n)*e;
+    number_of_samples = 100;
+    sample_frequency = 2;
+    r_w_f_o_y_e = .4;
+    allowable_risk = 1;
 
-benchmark_x0 = x0;  
-benchmark_x = x; 
+    trans_cost = .005;
+    wealth = 10000;
+    x0 = .3;
+    x = (.7/n)*e;
 
-rate_of_decay = 1 - r_w_f_o_y_e^(sample_frequency/52);
+    benchmark_x0 = x0;  
+    benchmark_x = x; 
 
-initial_wealth = wealth
-benchmark_wealth = wealth;	
+    rate_of_decay = 1 - r_w_f_o_y_e^(sample_frequency/52);
 
-rebalance_dates = start + horizon*(0:number_rebalances-1);
+    initial_wealth = wealth;
+    benchmark_wealth = wealth;	
 
-for i = 1:1
+    rebalance_dates = start + horizon*(0:number_rebalances-1);
 
-	trade_date = rebalance_dates(i);
-		
-    [mu,V] = adapted_stats(Price,trade_date, ...
-    		horizon,sample_frequency,number_of_samples,rate_of_decay);
-    mu0 = (1+.01*risk_free_rate(trade_date-1))^(horizon/52) - 1;
+    hist_cvar = zeros(1,length(rebalance_dates));
+    hist_benchmark = zeros(1,length(rebalance_dates));
+    hist_mvo = zeros(1,length(rebalance_dates));
     
-% :::::::::: ::::    ::: ::::::::::: :::::::::: :::::::::   ::::::::::: :::::::::: :::    ::: :::::::::::  :::    ::: :::::::::: :::::::::  :::::::::: ::: 
-% :+:        :+:+:   :+:     :+:     :+:        :+:    :+:      :+:     :+:        :+:    :+:     :+:      :+:    :+: :+:        :+:    :+: :+:        :+: 
-% +:+        :+:+:+  +:+     +:+     +:+        +:+    +:+      +:+     +:+         +:+  +:+      +:+      +:+    +:+ +:+        +:+    +:+ +:+        +:+ 
-% +#++:++#   +#+ +:+ +#+     +#+     +#++:++#   +#++:++#:       +#+     +#++:++#     +#++:+       +#+      +#++:++#++ +#++:++#   +#++:++#:  +#++:++#   +#+ 
-% +#+        +#+  +#+#+#     +#+     +#+        +#+    +#+      +#+     +#+         +#+  +#+      +#+      +#+    +#+ +#+        +#+    +#+ +#+        +#+ 
-% #+#        #+#   #+#+#     #+#     #+#        #+#    #+#      #+#     #+#        #+#    #+#     #+#      #+#    #+# #+#        #+#    #+# #+#            
-% ########## ###    ####     ###     ########## ###    ###      ###     ########## ###    ###     ###      ###    ### ########## ###    ### ########## ### 
-    
-    scenario = xlsread('scenario.xlsx');
-    mu = sum(scenario)/size(scenario,1);
-    
-    benchmark_risk = sqrt(e'*V*e)/(n+1);	
-    sigma = allowable_risk*benchmark_risk; 
-    
-    xx0 = x0;
-    xx = x;
-    [x0,x] = optimize_cvar(mu0,mu,V,sigma,xx0,xx,trans_cost);
-    wealth = wealth*(x0 + sum(x));
-    	
-    total = x0 + sum(x);	
-    x0 = x0/total;
-    x = x/total;
-    	
-    benchmark_wealth = benchmark_wealth*(1- trans_cost * sum(abs(benchmark_x-ones(n,1)/(n+1))));
-    	  
-    returns = (Price(trade_date+horizon-1,:)-Price(trade_date-1,:))./Price(trade_date-1,:);
-    
-    multiplier = 1 + mu0*x0 + returns*x;	
-    wealth = multiplier*wealth;
 
-    if wealth<=0
-        break;
+    for i = 1:length(rebalance_dates)
+
+        trade_date = rebalance_dates(i);
+
+        [mu,V] = adapted_stats(Price,trade_date, ...
+                horizon,sample_frequency,number_of_samples,rate_of_decay);
+        mu0 = (1+.01*risk_free_rate(trade_date-1))^(horizon/52) - 1;
+
+        scenario = xlsread('scenario.xlsx');
+        mu = sum(scenario)/size(scenario,1);
+
+        xx0 = x0;
+        xx = x;
+        [x0,x] = optimize_cvar(mu0,mu,V,xx0,xx,trans_cost);
+        wealth = wealth*(x0 + sum(x));
+
+        total = x0 + sum(x);	
+        x0 = x0/total;
+        x = x/total;
+
+        benchmark_wealth = benchmark_wealth*(1- trans_cost * sum(abs(benchmark_x-ones(n,1)/(n+1))));
+
+        hist_cvar(i) = wealth;
+        hist_benchmark(i) = benchmark_wealth;
+
+        returns = (Price(trade_date+horizon-1,:)-Price(trade_date-1,:))./Price(trade_date-1,:);   
+        multiplier = 1 + mu0*x0 + returns*x;	
+        wealth = multiplier*wealth;
+
+        if wealth<=0
+            break;
+        end
+
+        x0 = (1+mu0)*x0/multiplier;
+        x = x.*(1+returns)'/multiplier;
+
+        benchmark_multiplier = 1 + (mu0 + sum(returns))/(n+1);
+        benchmark_wealth = benchmark_multiplier*benchmark_wealth;
+
+        benchmark_x0 = ((1 + mu0)/(n+1)) / benchmark_multiplier;
+        benchmark_x = ((1 + returns')/(n+1))/ benchmark_multiplier;
+
     end
     
-    x0 = (1+mu0)*x0/multiplier;
-    x = x.*(1+returns)'/multiplier;
+    fprintf('your cvar wealth %f\n',wealth);
     
-    benchmark_multiplier = 1 + (mu0 + sum(returns))/(n+1);
-    benchmark_wealth = benchmark_multiplier*benchmark_wealth;
+    wealth = 10000;
+    x0 = .3;
+    x = (.7/n)*e;
     
-    benchmark_x0 = ((1 + mu0)/(n+1)) / benchmark_multiplier;
-    benchmark_x = ((1 + returns')/(n+1))/ benchmark_multiplier;
-   
+    
+    for i = 1:length(rebalance_dates)
+
+        trade_date = rebalance_dates(i);
+
+        [mu,V] = adapted_stats(Price,trade_date, ...
+                horizon,sample_frequency,number_of_samples,rate_of_decay);
+        mu0 = (1+.01*risk_free_rate(trade_date-1))^(horizon/52) - 1;
+
+        benchmark_risk = sqrt(e'*V*e)/(n+1);
+        sigma = allowable_risk*benchmark_risk; 
+
+        xx0 = x0;
+        xx = x;
+        [x0,x] =  cvx_markowitz(mu0,mu,V,sigma,xx0,xx,trans_cost);
+
+        wealth = wealth*(x0 + sum(x));
+
+        total = x0 + sum(x);	
+        x0 = x0/total;
+        x = x/total;
+
+        returns = (Price(trade_date+horizon-1,:)-Price(trade_date-1,:))./Price(trade_date-1,:);
+
+        multiplier = 1 + mu0*x0 + returns*x;	
+        wealth = multiplier*wealth;
+        hist_mvo(i) = wealth;
+
+        if wealth<=0
+            break;
+        end
+
+        x0 = (1+mu0)*x0/multiplier;
+        x = x.*(1+returns)'/multiplier;
+
+    end
+
+    fprintf('your mvo wealth %f\n',wealth);
+    fprintf('benchmark final wealth %f\n', benchmark_wealth);
+
+    plot(hist_cvar);
+    hold on
+    plot(hist_benchmark);
+    hold on
+    plot(hist_mvo);
+
 end
-
-fprintf('your final bank account %f\n');
-x0
-fprintf('your final risky portfolio %f\n');
-x
-
-fprintf('your final wealth %f\n',wealth);
-fprintf('benchmark final wealth %f\n', benchmark_wealth);
-
-fprintf('your annualized rate of return %f\n', (wealth/initial_wealth)^(52/(horizon*number_rebalances))-1);
-fprintf('benchmark annualized rate of return %f\n', (benchmark_wealth/initial_wealth)^(52/(horizon*number_rebalances))-1);
-
