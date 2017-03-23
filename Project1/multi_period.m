8dbaf64a274425362ded7424d3b354e1b87671bb
yhyhyh <893132051@qq.com>
2017-03-22 23:25:35 -0400

    Almost finish coding

--- Project1/multi_period.m
+++ Project1/multi_period.m
@@ -1,18 +1,15 @@
-function  multi_period
+%function  multi_period
 
     load hw2.mat; 
     n = size(Price,2);
     e = ones(n,1);
-
-    horizon = 4;
-    start = 400;
+    horizon = 2;
+    start = 250;
     number_rebalances = 100;
-
     number_of_samples = 100;
     sample_frequency = 2;
     r_w_f_o_y_e = .4;
     allowable_risk = 1;
-
     trans_cost = .005;
     wealth = 10000;
     x0 = .3;
@@ -20,9 +17,7 @@ function  multi_period
 
     benchmark_x0 = x0;  
     benchmark_x = x; 
-
     rate_of_decay = 1 - r_w_f_o_y_e^(sample_frequency/52);
-
     initial_wealth = wealth;
     benchmark_wealth = wealth;	
 
@@ -31,7 +26,8 @@ function  multi_period
     hist_cvar = zeros(1,length(rebalance_dates));
     hist_benchmark = zeros(1,length(rebalance_dates));
     hist_mvo = zeros(1,length(rebalance_dates));
-    
+    acc_cost1 = zeros(1,length(rebalance_dates));
+    acc_cost2 = zeros(1,length(rebalance_dates));
 
     for i = 1:length(rebalance_dates)
 
@@ -40,7 +36,9 @@ function  multi_period
         [mu,V] = adapted_stats(Price,trade_date, ...
                 horizon,sample_frequency,number_of_samples,rate_of_decay);
         mu0 = (1+.01*risk_free_rate(trade_date-1))^(horizon/52) - 1;
-
+        
+        % Get a scenario matrix from the nearest past date.
+        % Read the csv file and calculate expected return.
         nearest = floor(trade_date/50)*50;
         strcat(num2str(nearest),'.csv')
         scenario = csvread(strcat(num2str(nearest),'.csv'),1,1);
@@ -48,19 +46,22 @@ function  multi_period
 
         xx0 = x0;
         xx = x;
-        [x0,x] = optimize_cvar(mu0,mu,V,xx0,xx,trans_cost,0);
+        
+        % Call the core function to do the CVaR optimization.
+        [x0,x,cost] = optimize_cvar(mu0,mu,V,xx0,xx,trans_cost,0);
+        
+        % Keep track of transaction costs of every step.
+        acc_cost1(i) = acc_cost1(max(1,i-1))+cost;
+        
         wealth = wealth*(x0 + sum(x));
-
         total = x0 + sum(x);	
         x0 = x0/total;
         x = x/total;
 
-        benchmark_wealth = benchmark_wealth*(1- trans_cost * sum(abs(benchmark_x-ones(n,1)/(n+1))));
-
-        hist_cvar(i) = wealth;
-        hist_benchmark(i) = benchmark_wealth;
-
-        returns = (Price(trade_date+horizon-1,:)-Price(trade_date-1,:))./Price(trade_date-1,:);   
+        benchmark_wealth = benchmark_wealth*(1-trans_cost*sum(abs(...
+            benchmark_x-ones(n,1)/(n+1))));
+        returns = (Price(trade_date+horizon-1,:)-Price(trade_date-1,:))...
+            ./Price(trade_date-1,:);   
         multiplier = 1 + mu0*x0 + returns*x;	
         wealth = multiplier*wealth;
 
@@ -76,10 +77,14 @@ function  multi_period
 
         benchmark_x0 = ((1 + mu0)/(n+1)) / benchmark_multiplier;
         benchmark_x = ((1 + returns')/(n+1))/ benchmark_multiplier;
+        
+        hist_cvar(i) = wealth;
+        hist_benchmark(i) = benchmark_wealth;
 
     end
     
     fprintf('your cvar wealth %f\n',wealth);
+    fprintf('benchmark final wealth %f\n', benchmark_wealth);
     
     wealth = 10000;
     x0 = .3;
@@ -100,30 +105,27 @@ function  multi_period
         xx0 = x0;
         xx = x;
         [x0,x] =  cvx_markowitz(mu0,mu,V,sigma,xx0,xx,trans_cost);
+        acc_cost2(i) = acc_cost2(max(1,i-1))-x0-sum(x)+1;
 
         wealth = wealth*(x0 + sum(x));
-
         total = x0 + sum(x);	
         x0 = x0/total;
         x = x/total;
-
-        returns = (Price(trade_date+horizon-1,:)-Price(trade_date-1,:))./Price(trade_date-1,:);
+        returns = (Price(trade_date+horizon-1,:)-Price(trade_date-1,:))...
+            ./Price(trade_date-1,:);
 
         multiplier = 1 + mu0*x0 + returns*x;	
         wealth = multiplier*wealth;
         hist_mvo(i) = wealth;
-
         if wealth<=0
             break;
         end
 
         x0 = (1+mu0)*x0/multiplier;
         x = x.*(1+returns)'/multiplier;
-
     end
 
     fprintf('your mvo wealth %f\n',wealth);
-    fprintf('benchmark final wealth %f\n', benchmark_wealth);
 
     plot(hist_cvar);
     hold on
@@ -131,8 +133,14 @@ function  multi_period
     hold on
     plot(hist_mvo);
     
+    %plot(acc_cost1);
+    %hold on
+    %plot(acc_cost2);
+    RES = [hist_cvar', hist_benchmark', hist_mvo', acc_cost1', acc_cost2'];
+    csvwrite('result.csv',RES);
+    
     std(price2ret(hist_cvar))
     std(price2ret(hist_benchmark))
     std(price2ret(hist_mvo))
 
-end
+%end
--- Project1/optimize_cvar.m
+++ Project1/optimize_cvar.m
@@ -1,4 +1,25 @@
-function [x0,x] = optimize_cvar(mu0,mu,scenario,xx0,xx,trans_cost,type)
+%------------------------------------------------------------
+% Name: optimize cvar                                       |
+% Function: Give out weights and corresponding              |
+%     transaction cost based on all given information.      |
+%                                                           |
+% Input:                                                    |
+% - mu0: return rate of risk-free asset                     |
+% - mu: return rates of risky assets                        |
+% - scenario: scenario matrix from nearest past date        |
+% - xx0: weight of risk-free asset in the last period       |
+% - xx: weights of assets in the last period                |
+% - trans_cost: rate of transaction cost                    |
+% - type: indicate if take transaction cost into the        |
+%       optimization constraint.                            |
+%       (0 for no, 1 for yes)                               |
+%                                                           |
+% Output:                                                   |
+% - x0: optimized weight of risk-free asset                 |
+% - x: optimized weights vector of assets                   |
+% - cost: transaction cost for this step of rebalancing     |
+%------------------------------------------------------------
+function [x0,x,cost] = optimize_cvar(mu0,mu,scenario,xx0,xx,trans_cost,type)
     
     n = size(mu,2);
     lb = zeros(1,n+1);
@@ -11,16 +32,17 @@ function [x0,x] = optimize_cvar(mu0,mu,scenario,xx0,xx,trans_cost,type)
         beq = 1;
         X = fmincon(@(X)calc_CVaR(scenario,X),X0,A,b,Aeq,beq,lb,ub);
         sum(X)
+        cost = sum(abs(X-X0))*trans_cost;
+        X = X.*(1-cost);
     else
         X0 = [xx0; xx];
         XX = X0;
         X = fmincon(@(X)calc_CVaR(scenario,X),X0,[],[],[],[],lb,ub,...
             @(X)con_with_trans_cost(X,mu0,mu,trans_cost,XX));
         sum(X)
+        cost = 1-sum(X);
     end
     
-    total_trans_cost = sum(abs(X-X0))*trans_cost;
-    X = X.*(1-total_trans_cost);
     x0 = X(1);
     x = X(2:n+1);
     
@@ -31,5 +53,5 @@ function [c,ceq] = con_with_trans_cost(X,mu0,mu,trans_cost,XX)
     n = length(mu);
     cost = sum(abs(X-XX))*trans_cost;
     c = A*X+cost+1+sum(mu)/n*0.8;
-    ceq = X+cost-1;
+    ceq = sum(X)+cost-1;
 end
